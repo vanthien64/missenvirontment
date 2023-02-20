bf8e3fa9dfdf4b22c94451975ff896d58594062a
vanthien64 <vanthiendiendien@gmail.com>
2023-02-20 17:11:13 +0700

    init project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Login } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:"login",component:Login}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
