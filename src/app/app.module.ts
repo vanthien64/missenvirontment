55677da8963e33a4d72ca8bf0922f46466a6b5b5
vanthien64 <vanthiendiendien@gmail.com>
2023-02-21 17:10:37 +0700

    feat: homepage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Homepage } from './homepage/homepage.component';
 import { Login } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Login
+    Login,
+    Homepage
   ],
   imports: [
     BrowserModule,
